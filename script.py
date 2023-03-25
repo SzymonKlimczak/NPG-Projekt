0bcbc04162f46e92c3bae9f806c5c563c5c94801
MATEUSZxcv <mateusz.kuc@outlook.com>
2023-03-25 15:08:22 +0100

    podstawowe wysylanie wiadomosci

--- script.py
+++ script.py
@@ -1,16 +1,19 @@
 import smtplib, ssl
-port = 587 
-smtp_server = "smtp.gmail.com"
-sender_email = "buynever@gmail.com" #potrzeba zmienić uprawnienia
-receiver_email = "mkuc@student.agh.edu.pl"
-password = input("Hasło:")
+import getpass
+
+port = 465
+sender_email = input("Podaj email nadawcy (gmail): ")
+password = getpass.getpass("Podaj haslo i nacisniej ENTER: ")
+receiver_email = input("Podaj email odbiorcy: ")
+
 message = """\
 Subject: Test
-Automatyczna wiadomość."""
+
+Testujemy pythona"""
+
 context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo() 
-    server.starttls(context=context)
-    server.ehlo() 
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context = context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message) 
+    server.sendmail(sender_email, receiver_email, message)
+    print("Wyslano")
